9f501744bbcb5a05df3b21bf1ee44c95225acb0c
James Hepburn <jhepb059@uottawa.ca>
2026-03-12 10:19:36 -0600

    Initial Commit

---
+++ PYTHIA/PYTHIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PYTHIAApp.swift
+//  PYTHIA
+//
+//  Created by James Hepburn on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PYTHIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
